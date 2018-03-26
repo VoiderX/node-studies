305c797151aefda268d6e7b01da6c60c96aef581
Gabriel de Abreu <gabrieldeabreu.ti@gmail.com>
2018-03-26 17:46:32 -0300

    async basics

---
+++ weather-app/playground/async-basics.js
@@ -0,0 +1,8 @@
+console.log("Starting app");
+setTimeout(()=>{
+    console.log("Finished timer");
+},200)
+setTimeout(()=>{
+    console.log("Finished timer2");
+},0)
+console.log('Finishing app');
